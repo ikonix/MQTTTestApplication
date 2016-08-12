2c8d80c8f54db439afdd0e01e69ee4ea67bba442
awagn <awagn@X230>
2016-08-12 13:42:14 +0200

    Add project files.

---
+++ MqttTestAppl/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MqttTestAppl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MqttClient _cl;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public delegate void UpdateTextCallback(string message);
+        private void publishClick(object sender, RoutedEventArgs e)
+        {
+            _cl.publishTopic(topicTextBox.Text, messageTextBox.Text, MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+        }
+
+        private void subscribeClick(object sender, RoutedEventArgs e)
+        {
+            _cl.subscribeTopic(contentTextBox.Text.Split(), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE);
+        }
+
+        private void connectClick(object sender, RoutedEventArgs e)
+        {
+            _cl = new MqttClient(ipTextBox.Text);
+            _cl.returnSuccess += updateStatusTextBox;
+            _cl.messageReceived += setTextBox;
+        }
+
+        private void updateStatusTextBox(String text)
+        {
+            statusTextBox.Text = text;
+        }
+
+        private void setTextBox(String text)
+        {
+            if (!receivedTextBox.CheckAccess()) { 
+                receivedTextBox.Dispatcher.Invoke(new UpdateTextCallback(setTextBox), text);
+                return;
+            }
+
+            receivedTextBox.Text = text;
+        }
+    }
+}
---
+++ MqttTestAppl/MqttClient.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MqttTestAppl
+{
+    public class MqttClient
+    {
+        private uPLibrary.Networking.M2Mqtt.MqttClient client;
+
+        public event Action<String> returnSuccess;
+        public event Action<String> messageReceived;
+
+        public MqttClient(string ip)
+        {
+            //create client instance
+            client = new uPLibrary.Networking.M2Mqtt.MqttClient(ip);
+
+            client.MqttMsgPublishReceived += mqttMessageReceived;
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+        }
+
+        public uPLibrary.Networking.M2Mqtt.MqttClient Client {
+            get
+            {
+                return client;
+            }
+        }
+        
+        public void subscribeTopic(string[] topic, byte qosLevel)
+        {
+            byte[] qos = new byte[1];
+            qos[0] = qosLevel;
+            client.Subscribe(topic, qos);
+
+            returnSuccess("subscribe success");
+        }
+
+        public void publishTopic(string topic, string content, byte qosLevel, bool flag)
+        {
+            client.Publish(topic, Encoding.UTF8.GetBytes(content), qosLevel, flag);
+
+            returnSuccess("publish success");
+        }
+
+        private void mqttMessageReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string text = Encoding.UTF8.GetString(e.Message);
+            messageReceived(text);
+        }
+    }
+}
